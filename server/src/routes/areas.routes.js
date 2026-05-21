1ab50a75073951020e35b4c0ae7c00c37178f0cc
Halfa42 <A01738457@tec.mx>
2026-05-21 10:20:12 -0600

    Validacion y Schemas hechos

--- server/src/routes/areas.routes.js
+++ server/src/routes/areas.routes.js
@@ -1,13 +1,15 @@
 const router = require('express').Router()
 const { getAll, getById, create, update, remove } = require('../controllers/areas.controller')
 const { checkRole } = require('../middleware/authorize')
+const { validate } = require('../middleware/validate')
+const { createAreaSchema, updateAreaSchema } = require('../schemas/areas.schema')
 
 router.use(checkRole('admin')) 
 
 router.get('/', getAll)
 router.get('/:id', getById)
-router.post('/', create)
-router.put('/:id', update)
+router.post('/', validate(createAreaSchema), create)
+router.put('/:id', validate(updateAreaSchema), update)
 router.delete('/:id', remove)
 
 module.exports = router
--- server/src/routes/requests.routes.js
+++ server/src/routes/requests.routes.js
@@ -1,11 +1,15 @@
 const router = require('express').Router()
 const { getAll, getById, create, update, remove } = require('../controllers/requests.controller')
 const { isAuthenticated } = require('../middleware/auth')
+const { validate } = require('../middleware/validate')
+const { createRequestSchema, updateRequestSchema } = require('../schemas/requests.schema')
 
-router.get('/', isAuthenticated, getAll)
-router.get('/:id', isAuthenticated, getById)
-router.post('/', isAuthenticated, create)
-router.put('/:id', isAuthenticated, update)
-router.delete('/:id', isAuthenticated, remove)
+router.use(isAuthenticated)
+
+router.get('/', getAll)
+router.get('/:id', getById)
+router.post('/', validate(createRequestSchema), create)
+router.put('/:id', validate(updateRequestSchema), update)
+router.delete('/:id', remove)
 
 module.exports = router
